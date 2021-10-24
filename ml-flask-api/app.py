33df9db5e7ca3f65b84a3075b5194480b70257e3
Angelica Gardner <23157778+angelicagardner@users.noreply.github.com>
2021-10-24 12:30:47 +0200

    move load_dataset code into it's own file

--- ml-flask-api/app.py
+++ ml-flask-api/app.py
@@ -56,25 +56,8 @@ class MessageSchema(ma.Schema):
 message_schema = MessageSchema()
 messages_schema = MessageSchema(many=True)
 
-# from pathlib import Path
-# import pandas as pd
-# def load_data_into_DB():
-#     current_path = Path('.').resolve()
-#     data = pd.read_csv(current_path / '..' / 'data' / 'YInt_preprocessed.csv')
-#     for index, row in data.iterrows():
-#         try:
-#             print(index)
-#             db.session.add(Message(row['time'], row['location'], row['account'], row['original_message'],
-#                                    row['message'], row['hashtag'], row['mention'], row['is_repost'], row['number_reposts']))
-#             db.session.commit()
-#         except Exception as e:
-#             db.session.rollback()
-#             print(e)
-#         finally:
-#             db.session.close()
 
-
-# load_data_into_DB()
+# load_data_into_DB(db)
 
 
 @ v1.route('/messages', methods=['GET'])
