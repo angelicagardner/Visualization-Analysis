e40045ca325da97fb6bbc67b9169aac998f7b372
Omid Golshan <omid.gt21@gmail.com>
2021-11-07 20:04:23 +0100

    Add legend to the message timeline

--- dashboard/src/components/plots/TimeLine.jsx
+++ dashboard/src/components/plots/TimeLine.jsx
@@ -1,35 +1,43 @@
 import React, { useEffect, useState } from 'react';
 import Plot from 'react-plotly.js';
 import { TimeLineRepository } from '../../repositories/timeline.repository';
-import Moment from 'moment'
+import Moment from 'moment';
 
-function TimeLine({callback,timeRange}) {
-  const [timeline,setTimeline] = useState([])
-  
-  useEffect(()=>{
-    const repository = new TimeLineRepository()
-    const loadData = async (start,end) => {
+function TimeLine({ callback, timeRange }) {
+  const [timeline, setTimeline] = useState([]);
+
+  useEffect(() => {
+    const repository = new TimeLineRepository();
+    const loadData = async (start, end) => {
       let data;
 
-      if(start && end) data = (await repository.getRange(start,end)).data;
+      if (start && end) data = (await repository.getRange(start, end)).data;
       else data = (await repository.getAll()).data;
 
-      setTimeline( { x: data.map(item=>item.time), y:data.map(item=>item.numberOfMessages)})
-    }
-    
-    loadData(timeRange.start,timeRange.end)
-  },[timeRange])
+      setTimeline({
+        x: data.map((item) => item.time),
+        y: data.map((item) => item.numberOfMessages),
+      });
+    };
 
-  const updateHandler = (e)=>{
-    if(e['xaxis.range[0]'] && e['xaxis.range[1]']) callback(Moment(e['xaxis.range[0]']).format('YYYY-MM-DD HH:MM:ss'),Moment(e['xaxis.range[1]']).format('YYYY-MM-DD HH:MM:ss'))
+    loadData(timeRange.start, timeRange.end);
+  }, [timeRange]);
+
+  const updateHandler = (e) => {
+    if (e['xaxis.range[0]'] && e['xaxis.range[1]'])
+      callback(
+        Moment(e['xaxis.range[0]']).format('YYYY-MM-DD HH:MM:ss'),
+        Moment(e['xaxis.range[1]']).format('YYYY-MM-DD HH:MM:ss')
+      );
   };
 
   return (
-      <Plot
-        data={[{
-          x:timeline.x,
-          y:timeline.y,
-          name: 'control',
+    <Plot
+      data={[
+        {
+          x: timeline.x,
+          y: timeline.y,
+          name: 'Messages',
           autobinx: true,
           histnorm: 'count',
           marker: {
@@ -41,44 +49,48 @@ function TimeLine({callback,timeRange}) {
           },
           opacity: 0.5,
           type: 'histogram',
-        }]}
-        layout={{
-          responsive: true,
-          showlegend:false,
-          bargap: 0.02,
-          bargroupgap: 0.2,
-          barmode: 'stack',
-          title: {
-            text:'Message Distribution',
-          },
-          margin:{l:25, r:25, t:35, b:35},
-          paper_bgcolor:'transparent',
-          plot_bgcolor:'transparent',
-          yaxis: {
-            gridcolor: "rgba(255,255,255,0.25)",
-            gridwidth: 1,
-            zerolinecolor: "rgba(255,255,255,0.5)",
-            zerolinewidth: 1,
-          },
-          xaxis: {
-            gridcolor: "rgba(255,255,255,0.25)",
-            gridwidth: 1,  
-            zerolinecolor: "rgba(255,255,255,0.5)",
-            zerolinewidth: 1, 
-          },
-          font:{color:'white'}
-        }}
-        config={{
-          frameMargins:0,
-        }}
-        style={{
-          maxHeight:200,
-          margin:0,
-          padding:0,
-        }}
-        onRelayout={updateHandler}
-        onDoubleClick={()=>callback(undefined,undefined)}
-      />  );
+        },
+      ]}
+      layout={{
+        bargap: 0.02,
+        bargroupgap: 0.2,
+        barmode: 'stack',
+        title: {
+          text: 'Message Distribution',
+        },
+        margin: { l: 30, r: 30, t: 45, b: 45 },
+        paper_bgcolor: 'transparent',
+        plot_bgcolor: 'transparent',
+        yaxis: {
+          gridcolor: 'rgba(255,255,255,0.25)',
+          gridwidth: 1,
+          zerolinecolor: 'rgba(255,255,255,0.5)',
+          zerolinewidth: 1,
+        },
+        xaxis: {
+          gridcolor: 'rgba(255,255,255,0.25)',
+          gridwidth: 1,
+          zerolinecolor: 'rgba(255,255,255,0.5)',
+          zerolinewidth: 1,
+        },
+        font: { color: 'white' },
+        autosize: true,
+        responsive: true,
+        showlegend: true,
+        legend: {
+          orientation: 'h',
+          y: 100,
+        },
+      }}
+      style={{
+        maxHeight: 200,
+        margin: 0,
+        padding: 0,
+      }}
+      onRelayout={updateHandler}
+      onDoubleClick={() => callback(undefined, undefined)}
+    />
+  );
 }
 
 export default TimeLine;
