6db0e9c7f97ea4bb980bbbcd85fef3368849003a
Angelica Gardner <23157778+angelicagardner@users.noreply.github.com>
2021-11-09 23:32:02 +0100

    Implement TF-IDF algorithm

--- ml-flask-api/app.py
+++ ml-flask-api/app.py
@@ -19,24 +19,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 v1 = Blueprint('v1', __name__, url_prefix='/api/v1')
-v2 = Blueprint('v2', __name__, url_prefix='/api/v2')
 
 # Init db
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+# Message model
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     time = db.Column(db.String(100), nullable=False)
     location = db.Column(db.String(100), nullable=False)
     account = db.Column(db.String(100), nullable=False)
     message = db.Column(db.String(100), nullable=False)
+    tags = db.relationship('Tag', backref="message", lazy='dynamic')
 
-    def __init__(self, time, location, account, original_message):
+    def __init__(self, time, location, account, message):
         self.time = time
         self.location = location
         self.account = account
-        self.message = original_message
+        self.message = message
 
     def __repr__(self):
         return '<Message %r>' % self.id
@@ -48,11 +49,26 @@ class MessageSchema(ma.Schema):
 message_schema = MessageSchema()
 messages_schema = MessageSchema(many=True)
 
-# NLP
-nlp = spacy.load('en_core_web_sm')
-# Update spaCy's default stopwords list
-stop_words = ["'s", "m", "u", "o", "s"]
-nlp.Defaults.stop_words.update(stop_words)
+# Tf-Idf Model
+class Tag(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    word = db.Column(db.String(100), nullable=False)
+    weight = db.Column(db.Float, nullable=False)
+    message_id = db.Column(db.Integer, db.ForeignKey('message.id'), nullable=False)
+
+    def __init__(self, word, weight):
+        self.word = word
+        self.weight = weight
+
+    def __repr__(self):
+        return '<Tag %r>' % self.id
+
+class TagSchema(ma.Schema):
+    class Meta:
+        fields = ('word', 'weight')
+
+tag_schema = TagSchema()
+tags_schema = TagSchema(many=True)
 
 """
     Endpoint: /messages
@@ -60,7 +76,6 @@ nlp.Defaults.stop_words.update(stop_words)
     Returns data in JSON format.
 """
 @v1.route('/messages', methods=['GET'])
-@v2.route('/messages', methods=['GET'])
 def get_messages():
     start_date = request.args.get('start', default=None, type=str)
     end_date = request.args.get('end', default=None, type=str)
@@ -75,6 +90,85 @@ def get_messages():
     result = messages_schema.dump(all_messages)
 
     return jsonify(result)
+
+"""
+    Get a Bag-of-Words representation of all messages or all messages between a specified date range.
+"""
+@v1.route('/bow', methods=['GET'])
+def get_bow():
+    start_date = request.args.get('start', default=None, type=str)
+    end_date = request.args.get('end', default=None, type=str)
+    bow = {}
+
+    if start_date and end_date:
+        messages = Message.query.filter(
+            Message.time.between(start_date, end_date),Message.is_repost == False).all()
+    else:
+        messages = Message.query.filter(
+            Message.is_repost == False).all()
+
+    for message in messages:
+        if message.message:
+            words = nlp(message.message)
+            for word in words:
+                if not word.is_stop and not word.is_punct and not word.is_space and not word.is_digit:
+                    if word.text.lower() in bow:
+                        bow[word.text.lower()] += 1
+                    else:
+                        bow[word.text.lower()] = 1
+
+    bow = sorted(bow.items(), key=lambda x: x[1], reverse=True)
+
+    return jsonify(bow)
+
+"""
+    Count words in messages and group them by similar word patterns to infer topics.
+    All messages are used or only messages between a specified date range.
+    If predefined labels for topics are provided, messages will be classified into the most likely topic.
+"""
+@v1.route('/topics', methods=['GET'])
+def get_topics():
+    nlp = spacy.load('en_core_web_sm')
+    # Update spaCy's default stopwords list
+    stop_words = ["'s", "m", "u", "o", "s"]
+    nlp.Defaults.stop_words.update(stop_words)
+
+    params = request.args.to_dict(flat=False)
+    try:
+        num_topics = int(params.get('num_topics')[0])
+    except:
+        num_topics = 10
+    try:
+        start_date = params['start'][0]
+        end_date = params['end'][0]
+    except KeyError:
+        start_date = None
+        end_date = None
+    topics = {}
+
+    if start_date and end_date:
+        messages = Message.query.filter(
+            Message.time.between(start_date, end_date),Message.is_repost == False).all()
+    else:
+        messages = Message.query.filter(Message.is_repost == False).all()
+
+    doc = []
+    for message in messages:
+        if message.message:
+            words = nlp(message.message.lower())
+            text = []
+            for word in words:
+                if not word.is_stop and not word.is_punct and not word.is_space and not word.is_digit:
+                    text.append(word.lemma_)
+            doc.append(text)
+
+    dictionary = Dictionary(doc)
+    corpus = [dictionary.doc2bow(text) for text in doc]
+    lda = LdaModel(corpus=corpus, id2word=dictionary, num_topics=num_topics)
+
+    topics = (lda.print_topics(num_topics=num_topics))
+
+    return jsonify(topics)
 
 @v1.route('/locations', methods=['GET'])
 def get_locations():
@@ -114,89 +208,8 @@ def get_timeline():
     return jsonify(result)
 
 
-@v1.route('/bow', methods=['GET'])
-def get_bow():
-    """
-    Get a Bag-of-Words representation of all messages or all messages between a specified date range.
-    """
-    start_date = request.args.get('start', default=None, type=str)
-    end_date = request.args.get('end', default=None, type=str)
-    bow = {}
-
-    if start_date and end_date:
-        messages = Message.query.filter(
-            Message.time.between(start_date, end_date),Message.is_repost == False).all()
-    else:
-        messages = Message.query.filter(
-            Message.is_repost == False).all()
-
-    for message in messages:
-        if message.message:
-            words = nlp(message.message)
-            for word in words:
-                if not word.is_stop and not word.is_punct and not word.is_space and not word.is_digit:
-                    if word.text.lower() in bow:
-                        bow[word.text.lower()] += 1
-                    else:
-                        bow[word.text.lower()] = 1
-
-    bow = sorted(bow.items(), key=lambda x: x[1], reverse=True)
-
-    return jsonify(bow)
-
-
-@v1.route('/topics', methods=['GET'])
-def get_topics():
-    """
-    Count words in messages and group them by similar word patterns to infer topics.
-    All messages are used or only messages between a specified date range.
-    If predefined labels for topics are provided, messages will be classified into the most likely topic.
-    """
-    params = request.args.to_dict(flat=False)
-    try:
-        num_topics = int(params.get('num_topics')[0])
-    except:
-        num_topics = 10
-    try:
-        start_date = params['start'][0]
-        end_date = params['end'][0]
-    except KeyError:
-        start_date = None
-        end_date = None
-    topics = {}
-
-    # TODO: Implement topic classification
-    # if 'labels' in params.keys():
-    #     labels = params['labels'][0].split(',')
-
-    if start_date and end_date:
-        messages = Message.query.filter(
-            Message.time.between(start_date, end_date),Message.is_repost == False).all()
-    else:
-        messages = Message.query.filter(Message.is_repost == False).all()
-
-    doc = []
-    for message in messages:
-        if message.message:
-            words = nlp(message.message.lower())
-            text = []
-            for word in words:
-                if not word.is_stop and not word.is_punct and not word.is_space and not word.is_digit:
-                    text.append(word.lemma_)
-            doc.append(text)
-
-    dictionary = Dictionary(doc)
-    corpus = [dictionary.doc2bow(text) for text in doc]
-    lda = LdaModel(corpus=corpus, id2word=dictionary, num_topics=num_topics)
-
-    topics = (lda.print_topics(num_topics=num_topics))
-
-    return jsonify(topics)
-
-
 # Register version
 app.register_blueprint(v1)
-app.register_blueprint(v2)
 
 # Run server
 if __name__ == "__main__":
