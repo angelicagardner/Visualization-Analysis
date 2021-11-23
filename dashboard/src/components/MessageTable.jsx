071c98efe36a51bfe5c373a7249ef4a08884845d
Omid Golshan <omid.gt21@gmail.com>
2021-11-23 18:24:57 +0100

    Create a simple table for messages

---
+++ dashboard/src/components/MessageTable.jsx
@@ -0,0 +1,31 @@
+function MessageTable() {
+  return (
+    <div className="messageTable">
+      <div className="table-title">
+        <h3>Data Table</h3>
+      </div>
+      <table className="table-fill">
+        <thead>
+          <tr>
+            <td>Time</td>
+            <td>Location</td>
+            <td>User</td>
+            <td>Messages</td>
+            <td>Tags</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>2</td>
+            <td>3</td>
+            <td>4</td>
+            <td>5</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default MessageTable;
