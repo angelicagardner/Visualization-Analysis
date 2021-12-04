c3039b8f1462f20185ab8d6f0879012f441fb5b1
Omid Golshan <omid.gt21@gmail.com>
2021-12-04 14:36:15 +0100

    Increase the size of Ridgeline plot and improve the style

--- dashboard/src/components/plots/Ridgeline.jsx
+++ dashboard/src/components/plots/Ridgeline.jsx
@@ -1,21 +1,42 @@
+import { useEffect, useState, useRef } from 'react';
 import Plot from 'react-plotly.js';
 import { DataService } from '../../services/data-service';
 
 function RidgeLine({ data }) {
+  const parent = useRef(null);
   const keywords = DataService.getKeywordWeights(data);
-  const traces = Object.keys(keywords).map((i) => ({
-    points: 'none',
-    scalemode: 'width',
-    side: 'positive',
-    type: 'violin',
-    y0: i,
-    x: keywords[i],
-    orientation: 'h',
-    showlegend: false,
-  }));
+  const traces = Object.keys(keywords)
+    .map((i) => ({
+      points: false,
+      scalemode: 'width',
+      side: 'positive',
+      type: 'violin',
+      y0: i,
+      x: keywords[i],
+      orientation: 'h',
+      showlegend: false,
+      width: 3,
+      hoverinfo: 'x',
+      spanmode: 'soft',
+    }))
+    .filter((v) => v.x.length > 1) // remove occurrence less than 1
+    .sort((a, b) => b.x.length - a.x.length) // Sort based on number of values
+    .slice(0, 10); // Show only the first 10
+
+  const [layout, setLayout] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    setLayout({
+      width: parent?.current?.clientWidth ?? 0,
+      height: parent?.current?.clientHeight ?? 0,
+    });
+  }, [data]);
 
   return (
-    <div className="ridgeLine">
+    <div className="ridgeLine" ref={parent}>
       <Plot
         data={traces}
         layout={{
@@ -33,9 +54,11 @@ function RidgeLine({ data }) {
           },
           autosize: true,
           responsive: true,
-          margin: { l: 100, r: 25, b: 5, t: 5, pad: 5 },
+          margin: { l: 60, r: 45, b: 35, t: 5, pad: 0 },
           paper_bgcolor: 'transparent',
           plot_bgcolor: 'transparent',
+          height: layout.height,
+          width: layout.width,
         }}
       />
     </div>
