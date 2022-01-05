9be98fb05828f16820f565a32a2714a86b3c7b98
Omid Golshan <omid.gt21@gmail.com>
2022-01-05 11:02:21 +0100

    VA-62 Clean up 🧹✨

--- dashboard/src/components/plots/SunBurst.jsx
+++ dashboard/src/components/plots/SunBurst.jsx
@@ -57,7 +57,6 @@ function SunBurst({ filters, data, update }) {
   };
 
   const [width, height] = useWindowSize();
-  console.log(width, height);
 
   return (
     <div style={{ width: '100%', height: '100%' }} ref={parent}>
