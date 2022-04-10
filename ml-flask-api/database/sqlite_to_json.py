a3df97c27cf4f280dab85b6a88a414e74b77c361
Angelica Gardner <23157778+angelicagardner@users.noreply.github.com>
2022-04-10 14:23:11 +0200

    add SQLite to JSON conversion

---
+++ ml-flask-api/database/sqlite_to_json.py
@@ -0,0 +1,48 @@
+import sqlite3
+import json
+from pathlib import Path
+
+
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+def openConnection(pathToSqliteDb):
+    connection = sqlite3.connect(pathToSqliteDb)
+    connection.row_factory = dict_factory
+    cursor = connection.cursor()
+    return connection, cursor
+
+
+def getAllRecordsInTable(table_name, pathToSqliteDb):
+    conn, curs = openConnection(pathToSqliteDb)
+    conn.row_factory = dict_factory
+    curs.execute("SELECT * FROM '{}' ".format(table_name))
+    # fetchall as result
+    results = curs.fetchall()
+    # close connection
+    conn.close()
+    return json.dumps(results)
+
+
+def sqliteToJson(pathToSqliteDb):
+    connection, cursor = openConnection(pathToSqliteDb)
+    # select all the tables from the database
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    tables = cursor.fetchall()
+    # for each of the tables , select all the records from the table
+    for table_name in tables:
+        # Get the records in table
+        results = getAllRecordsInTable(table_name['name'], pathToSqliteDb)
+
+        # generate and save JSON files with the table name for each of the database tables and save in results folder
+        with open(Path(__file__).parent / 'YInt.json', 'w') as the_file:
+            the_file.write(results)
+    # close connection
+    connection.close()
+
+
+if __name__ == '__main__':
+    sqliteToJson(Path(__file__).parent / 'YInt.sqlite')
